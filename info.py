a1b57025d9a8b1b28c8816695efebb72e2e5ebb1
rajannjandan <121632208+rajannjandan@users.noreply.github.com>
2024-07-07 08:18:23 +0530

    Update info.py

--- info.py
+++ info.py
@@ -51,8 +51,8 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Zorosama:Zorosama@zorosama.r6ugscy.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Zorosama")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Zorotest:Zorotest@cluster0.ylsvoya.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Zorotest")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Premium And Referal Settings
